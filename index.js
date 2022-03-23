90c1131923dff141f22aca40198fecf8b52349c9
kotaro6969 <102255071+kotaro6969@users.noreply.github.com>
2022-03-23 20:19:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-m
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+require('module-alias/register')
+require('discord-buttons')(client);
+require('dotenv').config()
+require('discord-reply')
+
+const loadCommands = require('@root/commands/load-commands')
+const loadFeatures = require('@root/features/load-features')
+const { DiscordTogether } = require('discord-together')
+
+client.setMaxListeners(200)
+client.queue = new Map()
+client.discordTogether = new DiscordTogether(client)
+
+loadCommands(client)
+loadFeatures(client)
+
+client.login(process.env.token)
